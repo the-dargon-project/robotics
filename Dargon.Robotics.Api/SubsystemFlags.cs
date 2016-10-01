7b461a3ed47b8a5f3788482b5256d34615e04f92
Warty <ItzWarty@gmail.com>
2016-10-01 10:39:17 -0700

    Collapse Dargon.Robotics projects into one bigger project and a beaglebone project.

--- Dargon.Robotics.Api/SubsystemFlags.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Dargon.Robotics {
-   [Flags]
-   public enum SubsystemFlags : uint {
-      None     = 0,
-      Drive    = 0x00000001U
-   }
-}
---
+++ src/Dargon.Robotics/IterativeRobot.cs
@@ -0,0 +1,43 @@
+﻿using Dargon.Commons;
+using Dargon.Robotics.Devices;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dargon.Robotics.Codebases.Iterative {
+   public class IterativeRobot : Robot {
+      private readonly IterativeRobotConfiguration configuration;
+      private readonly IterativeRobotUserCode userCode;
+      private readonly Gamepad gamepad;
+
+      public IterativeRobot(IterativeRobotConfiguration configuration, IterativeRobotUserCode userCode, Gamepad gamepad) {
+         this.configuration = configuration;
+         this.userCode = userCode;
+         this.gamepad = gamepad;
+      }
+
+      public void Run() {
+         RunAsync().Wait();
+      }
+
+      private async Task RunAsync() {
+         var stopwatch = new Stopwatch().With(x => x.Start());
+         var tickIntervalMillis = 1000 / configuration.IterationsPerSecond;
+         userCode.Setup();
+
+         while (true) {
+            stopwatch.Restart();
+            userCode.OnTick();
+            var timeElapsedMillis = stopwatch.ElapsedMilliseconds;
+            var timeToSleepMillis = tickIntervalMillis - timeElapsedMillis;
+            if (timeToSleepMillis > 1) {
+               Console.WriteLine($"Finished in {timeElapsedMillis}/{tickIntervalMillis} ms.");
+               Thread.Sleep((int)timeToSleepMillis);
+            } else if(timeToSleepMillis < 0) {
+               Console.Error.WriteLine($"Warning: Iteration took {-timeToSleepMillis} millis too long to execute.");
+            }
+         }
+      }
+   }
+}
